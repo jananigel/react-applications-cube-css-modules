b14be2dde34ef7df09b98e39a21ef9da988d7ead
jananigel <jananigel@hotmail.com>
2021-05-26 15:00:42 +0800

    feat: useEffect remove event listener

--- src/components/application-cube.js
+++ src/components/application-cube.js
@@ -1,53 +1,7 @@
-import React, { setState, useState, useEffect } from "react";
-import ArrowButton from "../shared/components/buttons/arrow-btn.js";
-import styles from "./application-cube.module.scss";
-import Cube from "./cube/cube.js";
-
-// let timer;
-// let isIdle = false;
-
-// const runTime = () => {
-//   timer = setTimeout(() => {
-//     console.log("idle 3 seconds");
-//     clearTimeout(timer);
-//     isIdle = true;
-//   }, 3000);
-// };
-
-// const idleHandle = () => {
-//   runTime();
-//   document.addEventListener("mousemove", _ => {
-//     console.log("move");
-//     isIdle = false;
-//     clearTimeout(timer);
-//     timer = null;
-//     runTime();
-//   });
-
-//   document.addEventListener("click", _ => {
-//     clearTimeout(timer);
-//     isIdle = false;
-//     timer = null;
-//     runTime();
-//   });
-// };
-document.addEventListener("mousemove", _ => {
-  console.log("move");
-  // clearTimeout(timer);
-  // // setVariables({
-  // //   isIdle: false
-  // // });
-
-  // runTime();
-});
-
-document.addEventListener("click", _ => {
-  // clearTimeout(timer);
-  // setVariables({
-  //   isIdle: false
-  // });
-  // runTime();
-});
+import React, { useState, useEffect } from 'react';
+import ArrowButton from '../shared/components/buttons/arrow-btn.js';
+import styles from './application-cube.module.scss';
+import Cube from './cube/cube.js';
 
 const ApplicationCube = () => {
   let timer;
@@ -56,21 +10,36 @@ const ApplicationCube = () => {
   });
 
   const runTime = () => {
-    // timer = setTimeout(() => {
-    //   console.log("idle 3 seconds");
-    //   setVariables({
-    //     isIdle: true
-    //   });
-    //   clearTimeout(timer);
-    // }, 3000);
+    timer = setTimeout(() => {
+      console.log('idle 3 seconds');
+      setVariables({
+        isIdle: true
+      });
+      clearTimeout(timer);
+    }, 3000);
   };
 
   const idleHandle = () => {
     runTime();
   };
 
+  const clearIdle = () => {
+    clearTimeout(timer);
+    setVariables({
+      isIdle: false
+    });
+
+    runTime();
+  };
+
   useEffect(() => {
     // idleHandle();
+    // document.addEventListener('mousemove', clearIdle);
+    // document.addEventListener('click', clearIdle);
+    // return () => {
+    //   document.removeEventListener('click', clearIdle);
+    //   document.removeEventListener('mousemove', clearIdle);
+    // };
   });
 
   return (
@@ -78,10 +47,10 @@ const ApplicationCube = () => {
       <div className={`${styles.container}`}>
         <div className={`${styles.content}`}>
           <Cube isAutoPlay={variables.isIdle} />
-          <ArrowButton className={`${styles.top}`} panelClass={"top"} />
-          <ArrowButton className={`${styles.bottom}`} panelClass={"bottom"} />
-          <ArrowButton className={`${styles.left}`} panelClass={"left"} />
-          <ArrowButton className={`${styles.right}`} panelClass={"right"} />
+          <ArrowButton className={`${styles.top}`} panelClass={'top'} />
+          <ArrowButton className={`${styles.bottom}`} panelClass={'bottom'} />
+          <ArrowButton className={`${styles.left}`} panelClass={'left'} />
+          <ArrowButton className={`${styles.right}`} panelClass={'right'} />
         </div>
       </div>
     </>
